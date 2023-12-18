5b7a81f518f5e5bec66dc830670ee689b8b7a705
felipmartins <felipmartins94@gmail.com>
2023-12-18 19:14:34 -0300

    adicionano o meta dos modelos

--- portfolio/models.py
+++ portfolio/models.py
@@ -8,6 +8,10 @@ class Skill(models.Model):
         upload_to="portfolio/static/img/skill",
     )
 
+    class Meta:
+        verbose_name = "Habilidade"
+        verbose_name_plural = "Habilidades"
+
     def __str__(self):
         return self.title
 
@@ -16,6 +20,10 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     url = models.URLField()
+
+    class Meta:
+        verbose_name = "Projeto"
+        verbose_name_plural = "Projetos"
 
     def __str__(self):
         return self.title
@@ -27,6 +35,10 @@ class Education(models.Model):
     description = models.TextField()
     url = models.URLField()
 
+    class Meta:
+        verbose_name = "Educação"
+        verbose_name_plural = "Educações"
+
     def __str__(self):
         return self.title
 
@@ -35,6 +47,10 @@ class Experience(models.Model):
     company = models.CharField(max_length=100)
     role = models.CharField(max_length=100)
     start_end_date = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = "Experiência"
+        verbose_name_plural = "Experiências"
 
     def __str__(self):
         return self.role
@@ -45,6 +61,10 @@ class Recommendation(models.Model):
     role = models.CharField(max_length=100)
     company = models.CharField(max_length=100)
     description = models.TextField()
+
+    class Meta:
+        verbose_name = "Recomendação"
+        verbose_name_plural = "Recomendações"
 
     def __str__(self):
         return self.name
