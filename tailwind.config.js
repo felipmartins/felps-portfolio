557764007861b3e964d6d1d69a36dca3a88e9fb8
felipmartins <felipmartins94@gmail.com>
2023-12-18 16:15:49 -0300

    configuração do tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./portfolio/**/*.{html,js}"],
+  darkMode: "class",
   theme: {
     fontFamily: {
       merriweather: ['Merriweather', 'serif'],
@@ -8,9 +9,31 @@ module.exports = {
     extend: {
       backgroundImage: {
         'matrix-pattern': "url('./background.png')",
+        'sun': "url('./sun.jpeg')",
+        'moon': "url('./moon.jpeg')",
       },
+      colors: {
+        portPurple: '#2D1250',
+        portYellow: '#FFC857',
+      },
+      animation: {
+        moon: 'moonGrow 0.2s linear 0.2s',
+        sun: 'sunGrow 0.2s linear 0.2s',
+
     },
+      keyframes: {
+        moonGrow: {
+          '0%': { backgroundSize: '40px 40px' },
+          '50%': { backgroundSize: '38px 38px' },
+          '100%': { backgroundSize: '40px 40px' },
+        },
+        sunGrow: {
+          '0%': { backgroundSize: '40px 40px' },
+          '50%': { backgroundSize: '38px 38px' },
+          '100%': { backgroundSize: '40px 40px' },
+        },
+        },
   },
   plugins: [],
 }
-
+}
