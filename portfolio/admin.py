760d916bac72644a8c9dfe93771851df56ec9009
felipmartins <felipmartins94@gmail.com>
2023-12-18 18:57:55 -0300

    registra modelos no painel admin

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from portfolio.models import Project
+from portfolio.models import Skill, Project, Education, Experience, Recommendation
 
+admin.site.register(Skill)
 admin.site.register(Project)
+admin.site.register(Education)
+admin.site.register(Experience)
+admin.site.register(Recommendation)
