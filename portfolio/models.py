860a9fdbf7b7d4dffc4b5883f266886ca14a3c78
felipmartins <felipmartins94@gmail.com>
2023-12-18 18:57:06 -0300

    Implementa modelos do projeto

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,11 +1,50 @@
 from django.db import models
 
 
-class Project(models.Model):
+class Skill(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.ImageField(upload_to="portfolio/images/")
-    url = models.URLField(blank=True)
+    icon = models.ImageField(
+        upload_to="portfolio/static/img/skill",
+    )
 
     def __str__(self):
         return self.title
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    url = models.URLField()
+
+    def __str__(self):
+        return self.title
+
+
+class Education(models.Model):
+    title = models.CharField(max_length=100)
+    start_end_date = models.CharField(max_length=100)
+    description = models.TextField()
+    url = models.URLField()
+
+    def __str__(self):
+        return self.title
+
+
+class Experience(models.Model):
+    company = models.CharField(max_length=100)
+    role = models.CharField(max_length=100)
+    start_end_date = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.role
+
+
+class Recommendation(models.Model):
+    name = models.CharField(max_length=100)
+    role = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
