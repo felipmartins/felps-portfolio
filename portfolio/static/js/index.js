78b64e3bfceb84fbe1cbd3c16bc3705e1b9d0ae4
felipmartins <felipmartins94@gmail.com>
2023-12-18 16:15:40 -0300

    adiciona script para darkmode

---
+++ portfolio/static/js/index.js
@@ -0,0 +1,22 @@
+window.onload = () => {
+localStorage.theme = 'dark'
+    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark')
+  } else {
+    document.documentElement.classList.remove('dark')
+  }
+
+  const checkbox = document.querySelector('input[type="checkbox"]')
+  
+  checkbox.addEventListener('change', function(e) {
+    if (e.target.checked) {
+        document.documentElement.classList.remove('dark')
+        localStorage.theme = 'light'
+    } else {
+        document.documentElement.classList.add('dark')
+        localStorage.theme = 'dark'
+    }
+  })
+  
+}
