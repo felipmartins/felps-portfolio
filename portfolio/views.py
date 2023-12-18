740a2edd48028aa5e1b816d1f517f9f0708b2e99
felipmartins <felipmartins94@gmail.com>
2023-12-18 18:58:07 -0300

    cria view que renderiza e passa dados corretamente

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from portfolio.models import Skill, Project, Education, Experience, Recommendation
 
 
 def index(request):
-    return render(request, "index.html")
+    context = {
+        "skills": Skill.objects.all(),
+        "projects": Project.objects.all(),
+        "educations": Education.objects.all().order_by("-id"),
+        "experiences": Experience.objects.all().order_by("-id"),
+        "recommendations": Recommendation.objects.all(),
+    }
+    return render(request, "index.html", context)
