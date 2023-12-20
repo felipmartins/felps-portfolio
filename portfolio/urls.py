53348ae929401c394a7748e8b6a9cfcd71258662
felipmartins <felipmartins94@gmail.com>
2023-12-20 13:25:54 -0300

    adiciona urls e views

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,8 @@
-from portfolio.views import index
+from portfolio.views import index, convert_markdown_to_pdf, download_curriculo
 from django.urls import path
 
 urlpatterns = [
     path("", index, name="homepage"),
+    path("build-curricullum", convert_markdown_to_pdf, name="build"),
+    path("curriculum/download", download_curriculo, name="download"),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,16 @@
+import os
+
+from django.http import Http404, FileResponse
 from django.shortcuts import render
-from portfolio.models import Skill, Project, Education, Experience, Recommendation
+from portfolio.models import (
+    Skill,
+    Project,
+    Education,
+    Experience,
+    Recommendation,
+)
+from mdpdf.converter import Converter
+from django.http import HttpResponse
 
 
 def index(request):
@@ -11,3 +22,25 @@ def index(request):
         "recommendations": Recommendation.objects.all(),
     }
     return render(request, "index.html", context)
+
+
+def convert_markdown_to_pdf(request):
+    output_file = "staticfiles/curriculo.pdf"
+    markdown_file = "portfolio/curriculo.md"
+
+    Converter(output_file).convert([markdown_file])
+
+    return HttpResponse(
+        f"Curriculo gerado em {output_file}.", content_type="text/plain"
+    )
+
+
+def download_curriculo(request):
+    file_path = "staticfiles/curriculo.pdf"
+    if os.path.exists(file_path):
+        with open(file_path, "rb") as fh:
+            response = HttpResponse(fh.read(), content_type="application/pdf")
+            response["Content-Disposition"] = "attachment; filename=cv_felps.pdf"
+            return response
+
+    raise Http404
